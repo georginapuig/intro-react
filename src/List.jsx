8c7e9f7f32147a5426fcae658d80a78f49ea0375
georgina puig <georginaxcodes@gmail.com>
2020-07-20 18:48:26 -0500

    use a prop for the header

--- src/List.jsx
+++ src/List.jsx
@@ -28,7 +28,7 @@ class List extends React.Component {
     return (
       <div className="col-6 mx-auto">
         {/*Replace the code below to call the title prop*/}
-        <p className="h2">REPLACE THIS TITLE WITH A PROP</p>
+        <p className="h2">{this.props.title}</p>
         <form onSubmit={this.handleSubmit}>
           <label>
             <input
