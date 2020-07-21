fe998af3d0db0a300f07fe845816d18f2cd6e58b
georgina puig <georginaxcodes@gmail.com>
2020-07-21 01:31:19 -0500

    uncomment grades section

--- src/App.jsx
+++ src/App.jsx
@@ -80,7 +80,7 @@ class App extends React.Component {
     }
 
     /* Uncomment lines below to render grades*/
-    /*if (this.state.buttonClicked === "grades") {
+    if (this.state.buttonClicked === "grades") {
       tabChoice = (
         <Table
           tableNames={this.state.assignments}
@@ -89,7 +89,7 @@ class App extends React.Component {
           data={this.state.grades}
         />
       );
-    }*/
+    }
 
     return (
       <div>
